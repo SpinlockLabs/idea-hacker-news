bccfeefdac5b613fcfa3d83ea7627e77b355c164
Kenneth Endfinger <kaendfinger@gmail.com>
2021-04-04 00:00:25 -0400

    Migration to Gradle

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+# Hacker News IDEA
+
+## [Unreleased]
+
+## [1.3.0]
+
+### Added
+
+- Switched to standard JetBrains Gradle based plugin architecture.
+
+## [1.2.0]
+
+### Fixed
+
+- Fix bug where top stories were not properly supported.
+
+## [1.1.0]
+
+### Added
+
+- Added new plugin icon.
+
+## [1.0.0]
+
+### Added
+
+- The initial release with Top Stories support.
---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+import org.jetbrains.changelog.closure
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+  java
+
+  id("com.diffplug.spotless") version "5.11.0"
+  id("org.jetbrains.changelog") version "1.1.2"
+  id("org.jetbrains.intellij") version "0.7.2"
+}
+
+repositories {
+  mavenCentral()
+}
+
+project.group = properties("pluginGroup")
+project.version = properties("pluginVersion")
+
+intellij {
+  pluginName = "Hacker News"
+  version = properties("platformVersion")
+  type = properties("platformType")
+  downloadSources = properties("platformDownloadSources").toBoolean()
+  updateSinceUntilBuild = true
+}
+
+changelog {
+  version = properties("pluginVersion")
+  groups = emptyList()
+}
+
+tasks {
+  withType<JavaCompile> {
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
+  }
+
+  patchPluginXml {
+    version(properties("pluginVersion"))
+    sinceBuild(properties("pluginSinceBuild"))
+
+    changeNotes(
+      closure {
+        changelog.getLatest().toHTML()
+      }
+    )
+  }
+
+  runPluginVerifier {
+    ideVersions(properties("pluginVerifierIdeVersions"))
+  }
+
+  publishPlugin {
+    dependsOn("patchChangelog")
+    token(System.getenv("JETBRAINS_PUBLISH_TOKEN"))
+    channels(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first())
+  }
+}
+
+spotless {
+  kotlinGradle {
+    ktlint().userData(mapOf(
+      "indent_size" to "2",
+      "continuation_indent_size" to "2"
+    ))
+    endWithNewline()
+  }
+
+  java {
+    googleJavaFormat()
+  }
+}
+
+tasks.withType<Wrapper> {
+  gradleVersion = "6.8.3"
+  distributionType = Wrapper.DistributionType.ALL
+}
